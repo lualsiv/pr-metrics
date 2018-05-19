71c83b2cfa46f5abd657af6b66f5342b2ac8dc38
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-05-19 00:26:52 -0400

    Use Domain inside main()

--- lib/pr_metrics.ex
+++ lib/pr_metrics.ex
@@ -5,16 +5,19 @@ defmodule PrMetrics do
   require Logger
 
   @doc """
-  Entry point of the application. Not doing much (yet).
+  Log the average PRs open-time.
   """
   def main do
     # Instantiate the "I need to go out" adapters
+    today = fn -> Date.utc_today() |> Date.to_iso8601() end
+    prs = fn -> [] end
 
     # Instantiate the hexagon
+    avg_open_time = Domain.OpenTime.calculate(today, prs)
 
     # Instantiate the "I need to enter" adapters
 
     # App logic, using left-side adapters
-    Logger.info("Let's write some stuff here:\n")
+    Logger.info("Average open-time is: " <> to_string(avg_open_time))
   end
 end
