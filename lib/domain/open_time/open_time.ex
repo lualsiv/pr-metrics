3005755880a0c9b01fe0cddc9e78f27699d3c420
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-05-18 23:18:35 -0400

    Refactor domain architecture to reflect logical parts

---
+++ lib/domain/open_time/open_time.ex
@@ -0,0 +1,34 @@
+defmodule Domain.OpenTime do
+  @moduledoc """
+  Calculate Pull Requests (PRs) open-time.
+  """
+
+  @type pr :: %{created_at: String.t()}
+
+  @doc """
+  Returns the average number of days PRs have been opened until today.
+  """
+  @spec calculate(today :: (() -> String.t()), prs :: (() -> [pr])) :: non_neg_integer
+  def calculate(today, prs) do
+    prs_open_times_until(today.(), prs.())
+    |> average
+  end
+
+  @spec prs_open_times_until(today :: String.t(), prs :: [pr]) :: [non_neg_integer]
+  defp prs_open_times_until(today, prs) do
+    prs
+    |> Enum.map(fn pr -> pr.created_at end)
+    |> Enum.flat_map(fn created_at ->
+      case Domain.OpenTime.DaysDiff.between(today, created_at) do
+        {:ok, days_opened} -> [days_opened]
+        {:error, _} -> []
+      end
+    end)
+  end
+
+  @spec average(values :: [number]) :: [integer]
+  defp average(values) do
+    (Enum.sum(values) / Enum.count(values))
+    |> round
+  end
+end
--- test/domain/open_time/open_time_test.exs
+++ test/domain/open_time/open_time_test.exs
@@ -1,31 +1,12 @@
-defmodule Domain.OpenTimeCalculatorTest do
+defmodule Domain.OpenTimeTest do
   use ExUnit.Case
-  doctest Domain.OpenTimeCalculator
+  doctest Domain.OpenTime
 
   @today "2011-01-10T00:00:00Z"
-  @three_days_after "2011-01-13T00:00:00Z"
   @two_days_before "2011-01-08T00:00:00Z"
   @three_days_before "2011-01-07T00:00:00Z"
   @four_days_before "2011-01-06T00:00:00Z"
   @invalid_date "2011-32-00"
-
-  test "returns number of days opened until given date" do
-    {:ok, days_opened} = Domain.OpenTimeCalculator.days_opened(@today, @three_days_before)
-
-    assert days_opened == 3
-  end
-
-  test "fails if given date is before created date" do
-    result = Domain.OpenTimeCalculator.days_opened(@today, @three_days_after)
-
-    assert result == {:error, :created_date_in_the_future}
-  end
-
-  test "fails if given date has an invalid format" do
-    result = Domain.OpenTimeCalculator.days_opened(@today, @invalid_date)
-
-    assert result == {:error, :invalid_format}
-  end
 
   test "returns average number of days PRs have been opened until given date" do
     today = fn -> @today end
@@ -38,7 +19,7 @@ defmodule Domain.OpenTimeCalculatorTest do
       ]
     end
 
-    avg_open_time = Domain.OpenTimeCalculator.calculate(today, prs)
+    avg_open_time = Domain.OpenTime.calculate(today, prs)
 
     assert avg_open_time == 3
   end
@@ -55,7 +36,7 @@ defmodule Domain.OpenTimeCalculatorTest do
       ]
     end
 
-    avg_open_time = Domain.OpenTimeCalculator.calculate(today, prs)
+    avg_open_time = Domain.OpenTime.calculate(today, prs)
 
     assert avg_open_time == 3
   end
@@ -71,7 +52,7 @@ defmodule Domain.OpenTimeCalculatorTest do
       ]
     end
 
-    avg_open_time = Domain.OpenTimeCalculator.calculate(today, prs)
+    avg_open_time = Domain.OpenTime.calculate(today, prs)
 
     assert avg_open_time == 2
   end
